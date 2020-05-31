edd7093a8cfce6c43e42eff2ac1a393ac2168b19
haxul <s.s.starodubov@yandex.ru>
2020-05-31 16:49:22 +0400

    Main

--- src/lessons/Recursion.scala
+++ src/lessons/Recursion.scala
@@ -1,5 +1,11 @@
 package lessons
 
-object Recursion extends App {
+import scala.annotation.tailrec
 
+object Recursion extends App {
+  @tailrec
+  def helper(x : BigInt, acc:BigInt) :BigInt = {
+    if (x <= 1) acc
+    else helper(x - 1, x * acc)
+  }
 }
