40126df100057b6033c1c4c5f92cf9bd8b117e17
haxul <s.s.starodubov@yandex.ru>
2020-06-05 19:22:35 +0400

    add group

--- src/Hello.java
+++ src/Hello.java
@@ -1,16 +1,18 @@
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Hello {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(1,2,3,4,5);
-        List<String> strings = List.of("hello", "world");
+        List<String> strings = List.of("hello", "world", "my", "friend", "hello", "world");
 
         List<String> collect = strings.stream()
                 .flatMap((string) -> numbers.stream()
                         .filter((number -> number > 3))
                         .map(n -> string + " - " + n))
                 .collect(Collectors.toList());
-        System.out.println(collect);
+        Map<String, List<String>> collect1 = strings.stream().collect(Collectors.groupingBy((s) -> String.valueOf(s.charAt(0))));
+        
     }
 }
--- src/lessons/Collections.scala
+++ src/lessons/Collections.scala
@@ -2,5 +2,13 @@ package lessons
 
 object Collections extends App {
   val seq = Seq(1,2,3)
-
+//  (1 until  10) foreach print
+  val aList = List(1,2,3,4)
+  println(20 +: aList :+ 10)
+  val times = for {
+    i <- 1 to 10
+  } yield {
+    i + 10
+  }
+  println(times)
 }
