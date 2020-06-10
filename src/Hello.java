4c55b52477e5ab6106ccbcac1b6e6e6472a138db
haxul <s.s.starodubov@yandex.ru>
2020-06-10 19:05:03 +0400

    StringFormatter

--- src/Hello.java
+++ src/Hello.java
@@ -1,10 +1,11 @@
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Hello {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(1,2,3,4,5);
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
         List<String> strings = List.of("hello", "world", "my", "friend", "hello", "world");
 
         List<String> collect = strings.stream()
@@ -14,5 +15,30 @@ public class Hello {
                 .collect(Collectors.toList());
         Map<String, List<String>> collect1 = strings.stream().collect(Collectors.groupingBy((s) -> String.valueOf(s.charAt(0))));
 
+        String result = StringFormatter.unit("hello world")
+                .flatMap(x -> StringFormatter.unit(x.toUpperCase()))
+                .flatMap(x -> StringFormatter.unit(x.replace(" ", "!!!")))
+                .fold(x -> x);
+        System.out.println(result);
+    }
+}
+
+class StringFormatter {
+    private String a;
+
+    public StringFormatter(String a) {
+        this.a = a;
+    }
+
+    public StringFormatter flatMap(Function<String, StringFormatter> f) {
+        return f.apply(a);
+    }
+
+    public String fold(Function<String, String> f) {
+        return f.apply(a);
+    }
+
+    public static StringFormatter unit(String str) {
+        return new StringFormatter(str);
     }
 }
--- src/lessons/fp/StringApp.scala
+++ src/lessons/fp/StringApp.scala
@@ -1,5 +1,20 @@
 package lessons.fp
 
-object StringApp {
+object StringApp extends App {
 
+  class StringFormatter(str: String) {
+    def flatMap(f: String => StringFormatter): StringFormatter = f(str)
+
+    def fold(f: String => String): String = f(str)
+  }
+
+  object StringFormatter {
+    def apply(str: String): StringFormatter = new StringFormatter(str)
+  }
+
+  val s = StringFormatter(null)
+    .flatMap(s => StringFormatter(s.toUpperCase()))
+    .flatMap(s => StringFormatter(s.replace(" ", "!!!")))
+    .fold(s => s)
+  println(s)
 }
